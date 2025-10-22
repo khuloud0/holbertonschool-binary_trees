728d4bf5729cd99ed496c827c5715d98e5bdcf85
root <root@ip-10-42-112-9.ec2.internal>
2025-10-22 14:57:58 +0000

    Fix line length and Betty style in 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,12 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through a binary tree
+ * using post-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  *
- * Description: visits left subtree, then right subtree, then the node itself.
- * If tree or func is NULL, do nothing.
+ * Description: visits left subtree, then right subtree,
+ * and finally the node itself. If tree or func is NULL,
+ * the function does nothing.
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
