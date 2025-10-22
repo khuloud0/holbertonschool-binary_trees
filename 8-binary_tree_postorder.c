431d812ccb8a1a55caac0abed522a87c15824e1e
root <root@ip-10-42-112-9.ec2.internal>
2025-10-22 14:54:05 +0000

    Fix Betty style in 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - Goes through a binary tree using post-order traversal
- * @tree: Pointer to the root node of the tree to traverse
- * @func: Pointer to a function to call for each node
+ * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node
  *
- * Description: Post-order traversal means visiting Left -> Right -> Root.
+ * Description: visits left subtree, then right subtree, then the node itself.
  * If tree or func is NULL, do nothing.
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
