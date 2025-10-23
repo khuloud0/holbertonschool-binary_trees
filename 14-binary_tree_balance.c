5116e0f2a9693724a76eceece057bf0a5be95a1e
root <root@ip-10-42-175-11.ec2.internal>
2025-10-23 12:23:40 +0000

    Fix: correct binary_tree_balance height calculation

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,43 +1,43 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height_balance - measures height of a binary tree
- * @tree: pointer to the root node
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree
  *
- * Return: height, or 0 if tree is NULL
+ * Return: Height of the tree, or 0 if tree is NULL
  */
-size_t binary_tree_height_balance(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
+	size_t left_h = 0, right_h = 0;
 
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left)
-		left_height = 1 + binary_tree_height_balance(tree->left);
+		left_h = 1 + binary_tree_height(tree->left);
 	if (tree->right)
-		right_height = 1 + binary_tree_height_balance(tree->right);
+		right_h = 1 + binary_tree_height(tree->right);
 
-	return (left_height > right_height ? left_height : right_height);
+	return ((left_h > right_h) ? left_h : right_h);
 }
 
 /**
- * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: pointer to the root node
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
  *
- * Return: balance factor (left height - right height)
+ * Return: Balance factor, or 0 if tree is NULL
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height = 0, right_height = 0;
+	int left_h = 0, right_h = 0;
 
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left)
-		left_height = (int)binary_tree_height_balance(tree->left);
+		left_h = (int)binary_tree_height(tree->left);
 	if (tree->right)
-		right_height = (int)binary_tree_height_balance(tree->right);
+		right_h = (int)binary_tree_height(tree->right);
 
-	return (left_height - right_height);
+	return (left_h - right_h);
 }
